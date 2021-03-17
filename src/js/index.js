82b64ece954c3f9767ea751cfba2b1a2b5253402
wilsonw13 <71456315+wilsonw13@users.noreply.github.com>
2021-03-17 09:56:09 -0400

    installed parcel

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import {
+  randomInteger,
+  threeDigitInt,
+  twoDigitInt,
+  oneDigitInt,
+} from "./Functions";
+
+console.log(randomInteger(100, 1000));
+console.log(threeDigitInt());
+console.log(twoDigitInt());
+
+console.log("Bonjour");
