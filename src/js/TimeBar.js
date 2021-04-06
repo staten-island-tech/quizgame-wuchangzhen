aac4e3b0964c74a2cea535a6a19802dfa8742f21
kevinz5204 <kevin.zhang22@sitechhs.com>
2021-04-05 20:23:00 -0400

    Start of the Timebar
    tried to test some stuff but realized that my go live is all sorts of messed up, shows listing directory so i'll just ask whalen about it in class

---
+++ src/js/TimeBar.js
@@ -0,0 +1,7 @@
+import { DOMSelectors } from './Dom';
+
+function initBarCount() {
+  const divTimeLeft = document.getElementById('divTimeLeft');
+  const divCountdownBar = document.getElementById('divCountdownBar');
+  function barCount() {}
+}
