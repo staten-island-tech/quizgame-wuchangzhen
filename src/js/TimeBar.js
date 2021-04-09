c4853bcdb183877d7b471fe388f74394b27efda9
kevinz5204 <kevin.zhang22@sitechhs.com>
2021-04-09 00:56:06 -0400

    not working. want to cry
    - i think my sizing in my css is throwing off the width values that i should be getting
    - after make it work, figure out how to reverse it, and then weorry about more complex implementations like adding and subtracting time

--- src/js/TimeBar.js
+++ src/js/TimeBar.js
@@ -1,7 +1,13 @@
 import { DOMSelectors } from './Dom';
 
-function initBarCount() {
-  const divTimeLeft = document.getElementById('divTimeLeft');
-  const divCountdownBar = document.getElementById('divCountdownBar');
-  function barCount() {}
-}
+//basically a div over another div thats disappearing over the span of a specified length of time
+
+const timerBar = document.querySelector('timerBar');
+setInterval(() => {
+  const computedStyle = getComputedStyle(timerBar); //get current width and then increment based on percentage loaded-> get computedStyle and then use parsefloat to take width and convert to number -> get property value for --width
+  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+  //once page loads,no width supplied, not a number, defaulted to 0
+  timerBar.style.setProperty('--width', width + 0.1);
+}, 15); //calls on function every 5 seconds
+
+//parseFloat converts string to number, but why is this necessary?>????
